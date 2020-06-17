2418c26724679158ad11ca485f1c87a2c87821b7
hogehiko <takrockjp@gmail.com>
2020-06-17 20:28:53 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,26 @@
 
 #[macro_use]
 extern crate json;
+#[macro_use]
 extern crate serde_derive;
+#[macro_use]
 extern crate serde;
+#[macro_use]
 extern crate serde_json;
 
 use std::fs::File;
+use std::error::Error;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::io::SeekFrom;
+use std::io::{self, ErrorKind};
 use std::mem;
 
+use std::collections::BTreeMap;
+
 use clap::Clap;
+
+use serde::{Serialize, Deserialize};
 
 
 
@@ -49,6 +58,9 @@ struct Create {
 
     #[clap(long)]
     schema: String,
+
+    #[clap(long)]
+    primary: String,
 }
 
 #[derive(Clap)]
@@ -60,28 +72,30 @@ struct Query{
     eq: Option<String>
 }
 
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 enum DataType{
     Integer,
-    Str
+    Str // 256 bytes固定
 }
+
 
 
 impl DataType{
     fn from_string(value: &str) -> DataType{
-        if value == "Integer"{
+        if value == "i"{
             return DataType::Integer;
         }
         return DataType::Str;
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 struct Field{
     name: String,
     data_type: DataType,
 }
 
+#[derive(Serialize, Deserialize)]
 struct Schema{
     name: String,
     primary: Field,
@@ -89,22 +103,126 @@ struct Schema{
 }
 
 
-fn load(table: String, filename: String){
+impl Schema{
+    fn load(table: &str)->Result<Schema, Box<dyn std::error::Error>>{
+        let schema_str = std::fs::read_to_string(format!("{}.def", table))?;
+        match serde_json::from_str::<Schema>(&schema_str){
+            Ok(x)=> Ok(x),
+            Err(e) => Err(Box::new(e))
+        }
+    }
+}
+
+struct Record{
+    index: i64,
+    // size: i64
+}
+struct Table{
+    schema: Schema,
+    segments: Vec<Box<dyn Segment>>
 
 }
 
-fn create(name: String, schema: String){
-    let fields = to_field(&schema);
-    let s = Schema{
-        name: name,
+impl Table{
+    fn load(name: &str){
         
-        primary: fields[0].clone(),
-        fields: fields,
-    };
-    ()
+    }
 }
 
-fn to_field(schema: &String) -> Vec<Field>{
+trait Segment{
+    fn get(&self, index: i64)->Option<&Record>;
+}
+
+struct InMemorySegment{
+    data: BTreeMap<i64, Record>
+}
+
+impl Segment for InMemorySegment{
+    fn get(&self, index: i64)->Option<&Record> {
+        self.data.get(&index)
+
+    }
+}
+
+struct FileSegment{
+    bodyfile: File,
+    indexfile: File
+}
+
+// メモリに詰むための疎なinde
+// sort済みデータしか扱わないのでrebalance不要 ->　バイナリサーチで良い
+struct IndexEntry{
+    key: u64,
+    position: usize
+}
+struct Index{
+    index: Vec<IndexEntry>
+}
+
+impl Index{
+    fn find_nearest_position(&self, key:&u64)->usize{
+        fn _inner(index: &[IndexEntry], key: &u64)->usize{
+            let center = index.len() / 2;
+            if index.len() == 1{
+                index[0].position
+            }else if index[center].key == *key{
+                index[center].position
+            }else if index[center].key > *key{
+                _inner(&index[0..center-1], key)
+            }else{
+                _inner(&index[center..], key)
+            }
+        }
+        _inner(&self.index.as_slice(), key)
+    }
+}
+
+
+
+impl FileSegment{
+    // fn new(filepath: bodyfile_name, indexfile_name) {
+        
+    // }
+}
+
+impl Segment for FileSegment{
+    fn get(&self, index: i64)->Option<&Record> {
+        None
+    }
+}
+
+
+
+
+fn load(table: String, filename: String)->std::io::Result<()>{
+    let schema_str = std::fs::read_to_string(format!("{}.def", &table))?;
+    let schema = serde_json::from_str::<Schema>(&schema_str);
+    
+    Ok(())
+}
+
+fn create(name: &str, schema: &str, primary: &str)->std::io::Result<()> {
+    let fields = to_field(schema);
+    let primary_f = fields.iter().find(|a|a.name == *primary);
+    
+    match primary_f {
+        Some(x) => {
+            let s = Schema{
+                name: name.to_string(),
+                primary: x.clone(),
+                fields: fields,
+            };
+            let mut schema_file = File::create(format!("{}.def", name))?;
+            schema_file.write_all(serde_json::to_string(&s)?.as_bytes());
+            Ok(())
+        },
+        None => {
+            Err(std::io::Error::new(ErrorKind::InvalidInput, "aaa"))
+        }
+    }
+}
+
+fn to_field(schema: &str) -> Vec<Field>{
     let mut result = Vec::new();
     for f in schema.split(","){
        let fs = f.split(":").collect::<Vec<&str>>();
@@ -127,7 +245,7 @@ fn main() -> std::io::Result<()> {
         },
         SubCommand::Create(c) => {
             println!("Create {}", c.name);
-            create(c.name, c.schema);
+            create(&(c.name), &(c.schema), &(c.primary));
         },
     }
     // let f = File::open("data.json")?;
@@ -137,5 +255,15 @@ fn main() -> std::io::Result<()> {
     //     let obj = json::parse(&(line?));
     //     println!("{}", obj.unwrap());
     // }
+
+    let mut bt = BTreeMap::new();
+    bt.insert(3, "333");
+    bt.insert(1, "111");
+    bt.insert(2, "222");
+    bt.insert(-100, "222");
+
+    for (k,v) in bt.into_iter(){
+        println!("{}", k);
+    }
     Ok(())
 }
